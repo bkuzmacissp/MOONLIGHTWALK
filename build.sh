e15440f37cc0006fc60427ef2d4195240c9681bd
Bobby Kuzma <bkuzmacissp@gmail>
2017-06-24 09:15:16 -0400

    mods for the firewall deploy.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Starting Ansible Playbook for CTF rev 2.1
-ansible-playbook -i ansible/inventory/hosts -c ssh ansible/site.yml
+ansible-playbook --ask-pass -i ansible/inventory/hosts -c ssh ansible/site.yml
