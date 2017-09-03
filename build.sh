3e0bbd515d15a3e695fbe257957f7f805a0249e7
Bobby Kuzma <bkuzma@CORE.SEC>
2017-09-02 22:34:27 -0400

    d

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Starting Ansible Playbook for CTF rev 2.1
-ansible-playbook -e 'host_key_checking=False' -i ansible/inventory/hosts -c ssh ansible/site.yml
+ansible-playbook -vvvv -i ansible/inventory/hosts -c ssh ansible/site.yml
