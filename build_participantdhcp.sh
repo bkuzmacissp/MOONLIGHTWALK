c2b89629a6fa63f6e56913744fde9e6efae60b2f
Bobby Kuzma <bkuzma@CORE.SEC>
2017-09-09 09:04:39 -0400

    Correction

--- build_participantdhcp.sh
+++ build_participantdhcp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Starting Ansible Playbook for CTF rev 2.1
-ansible-playbook -vvvv -i ansible/inventory/hosts -c ssh ansible/participantdhcp.yml
+ansible-playbook -vvvv -i ansible/inventory/hosts -c ssh ansible/red_infras.yml
