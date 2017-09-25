3bb6c3df78e2a69ad95db19ad1728f9fd90e5eff
Bobby Kuzma <bkuzma@CORE.SEC>
2017-09-25 02:33:53 -0400

    Merged

--- ansible/files/Exchange-Finish.ps1
+++ ansible/files/Exchange-Finish.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 New-AcceptedDomain -DomainName mercytechhealthcare.com -DomainType Authoritative -Name "External Domain"
 Set-AcceptedDomain -Identity "External Domain" -MakeDefault $true
@@ -7,12 +6,3 @@ Update-EmailAddressPolicy -Identity "Default Policy"
 New-ManagementRoleAssignment -role "Mailbox import Export" -user "mercy\administrator"
 get-user | enable-mailbox
 Dir \\exchange\disk\mailboxes\*.pst | %{ New-MailboxImportRequest -Name RecoveredPST -BatchName Recovered -Mailbox $_.BaseName -FilePath $_.FullName -TargetRootFolder SubFolderInPrimary}
-=======
-Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
-New-AcceptedDomain -DomainName mercytechhealthcare.com -DomainType Authoritative -Name "External Domain"
-Set-AcceptedDomain -Identity "External Domain" -MakeDefault $true
-Set-EmailAddressPolicy -Identity "Default Policy" -EnabledPrimarySMTPAddressTemplate "SMTP:%g.%s@mercytechhealthcare.com"
-Update-EmailAddressPolicy -Identity "Default Policy"
-get-user | enable-mailbox
-Dir \\exchange\mailboxes\*.pst | %{ New-MailboxImportRequest -Name RecoveredPST -BatchName Recovered -Mailbox $_.BaseName -FilePath $_.FullName -TargetRootFolder SubFolderInPrimary}
->>>>>>> ca5640e15e506015290bf68d9eaa9aaa20860056
