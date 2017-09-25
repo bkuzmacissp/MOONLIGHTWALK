82852a2af9c08a3813df1253950dd1fbfc3d2cc7
Bobby Kuzma <bkuzma@CORE.SEC>
2017-09-25 02:32:18 -0400

    Updated infrastructure

--- ansible/files/Exchange-Finish.ps1
+++ ansible/files/Exchange-Finish.ps1
@@ -3,5 +3,6 @@ New-AcceptedDomain -DomainName mercytechhealthcare.com -DomainType Authoritative
 Set-AcceptedDomain -Identity "External Domain" -MakeDefault $true
 Set-EmailAddressPolicy -Identity "Default Policy" -EnabledPrimarySMTPAddressTemplate "SMTP:%g.%s@mercytechhealthcare.com"
 Update-EmailAddressPolicy -Identity "Default Policy"
+New-ManagementRoleAssignment -role "Mailbox import Export" -user "mercy\administrator"
 get-user | enable-mailbox
-Dir \\exchange\mailboxes\*.pst | %{ New-MailboxImportRequest -Name RecoveredPST -BatchName Recovered -Mailbox $_.BaseName -FilePath $_.FullName -TargetRootFolder SubFolderInPrimary}
+Dir \\exchange\disk\mailboxes\*.pst | %{ New-MailboxImportRequest -Name RecoveredPST -BatchName Recovered -Mailbox $_.BaseName -FilePath $_.FullName -TargetRootFolder SubFolderInPrimary}
